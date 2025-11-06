1932f509bcc5bc611590e6ff18b06ee3ffea9dce
Iana <iana@Shivakumars-MacBook-Pro.local>
2025-11-06 21:37:15 +0100

    tokean as argument

--- src/grasp_agents/telemetry/phoenix.py
+++ src/grasp_agents/telemetry/phoenix.py
@@ -25,7 +25,6 @@ def init_phoenix(
     api_token: str | None = None,
 ):
     collector_endpoint = os.getenv("TELEMETRY_COLLECTOR_HTTP_ENDPOINT")
-    api_token = os.getenv("PHOENIX_API_KEY")
 
     if not collector_endpoint:
         logger.warning(
